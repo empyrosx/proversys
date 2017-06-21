d22e9d6ec9796b62373ddb50ecbd1f5486ae647f
empyrosx <empyrosx@gmail.com>
2017-06-21 23:10:49 +0300

    Integration test for ProjectController.

---
+++ web/src/test/java/com/github/empyrosx/proversys/web/ProjectControllerIntegrationTest.java
@@ -0,0 +1,102 @@
+package com.github.empyrosx.proversys.web;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.github.empyrosx.proversys.model.Project;
+import com.github.empyrosx.proversys.service.ProjectService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Integration tests for project controller.
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration({"classpath:/spring/spring-mvc.xml"})
+@WebAppConfiguration
+@Configuration
+public class ProjectControllerIntegrationTest {
+
+    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+
+    static {
+        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
+        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    }
+
+    private MockMvc mockMvc;
+
+    private WebClient webClient;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private List<Project> projects = asList(new Project("Web-consolidation"),
+            new Project("Web-planning"));
+
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilter(CHARACTER_ENCODING_FILTER)
+                .build();
+
+        webClient = MockMvcWebClientBuilder
+                .mockMvcSetup(mockMvc)
+                .useMockMvcForHosts("proversys.ru")
+                .build();
+    }
+
+    @Bean
+    public ProjectService getService() {
+        ProjectService result = mock(ProjectService.class);
+        when(result.findAll()).thenReturn(projects);
+        return result;
+    }
+
+    @Test
+    public void projectPageIsRenderedAsHtmlWithListOfProjects() throws IOException {
+        HtmlPage page = webClient.getPage("http://proversys.ru/projects.htm");
+        List<String> booksList = page.getElementsByTagName("li")
+                .stream().map(DomNode::asText).collect(toList());
+        assertThat(booksList, hasItems("1. Web-consolidation", "2. Web-planning"));
+    }
+
+    @Test
+    public void requestForProjectsIsSuccessfullyProcessedWithAvailableProjectsList() throws Exception {
+        this.mockMvc.perform(get("/projects.htm")
+                .accept(MediaType.parseMediaType("text/html;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(allOf(
+                        containsString("Web-consolidation"),
+                        containsString("Web-planning")))
+                );
+    }
+}
